15af5b6f3462091b9fe73ba0fd33494c8493abc5
Juan <gurquiza8@gmail.com>
2022-11-20 22:25:30 -0600

    Primer commit de practica de flex, box y Position

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { SafeAreaView } from "react-native"
+import { TareaScreen } from "./src/screens/TareaScreen";
+// import { FlexScreen } from "./src/screens/FlexScreen";
+// import { DimensionesScreen } from "./src/screens/DimensionesScreen"
+// import { BoxObjectModelScreen } from "./src/screens/BoxObjectModelScreen"
+// import { ContadorScreen } from "./src/screens/ContadorScreen"
+// import { HolaMundoScreen } from "./src/screens/HolaMundoScreen"
+// import { PositionScreen } from './src/screens/PositionScreen';
+
+export const App = () => {
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} >
+      {/* <HolaMundoScreen /> */}
+      {/* <ContadorScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      {/* <DimensionesScreen /> */}
+      {/* <PositionScreen /> */}
+      {/* <FlexScreen /> */}
+      <TareaScreen/>
+    </SafeAreaView>
+  )
+}
+
